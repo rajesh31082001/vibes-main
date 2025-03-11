14dfdcd9b86bd3ae189fdd638e5e1e7295989134
rajesh31082001 <rajeshpal31082001@gmail.com>
2025-03-12 02:24:51 +0530

    cron done

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ import messageRoutes from "./routes/messageRoutes.js";
 // import chatRoutes from "./routes/chatRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
-// import job from "./cron/cron.js";
+import job from "./cron/cron.js";
 
 
 dotenv.config();
 
 connectDB();
-// job.start();
+job.start();
 
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
